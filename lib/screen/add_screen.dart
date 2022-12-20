62b4490381bd0c6d0b906da36331e13913e7f380
Jameakelan <akananyotkeawlamoon2@gmail.com>
2022-12-20 22:24:54 +0700

    add validate into add contact

--- lib/screen/add_screen.dart
+++ lib/screen/add_screen.dart
@@ -9,6 +9,13 @@ class AddScreen extends StatefulWidget {
 }
 
 class _AddScreenState extends State<AddScreen> {
+  final TextEditingController _textNameController = TextEditingController();
+  final TextEditingController _textMobileController = TextEditingController();
+  final TextEditingController _textEmailController = TextEditingController();
+
+  bool _validateNameEmpty = false;
+  bool _validateMobileEmpty = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,26 +39,32 @@ class _AddScreenState extends State<AddScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            const TextField(
+            TextField(
+              controller: _textNameController,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Name",
-                  prefixIcon: Icon(Icons.person)),
+                border: const OutlineInputBorder(),
+                labelText: "Name",
+                errorText: (_validateNameEmpty) ? "Name can't be null" : null,
+                prefixIcon: const Icon(Icons.person),
+              ),
             ),
             const SizedBox(
               height: 15,
             ),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+            TextField(
+              controller: _textMobileController,
+              decoration:  InputDecoration(
+                  border: const OutlineInputBorder(),
                   labelText: "Mobile",
-                  prefixIcon: Icon(Icons.phone)),
+                  errorText: (_validateMobileEmpty) ? "Mobile can't be null" : null,
+                  prefixIcon: const Icon(Icons.phone)),
             ),
             const SizedBox(
               height: 15,
             ),
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: _textEmailController,
+              decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: "E-mail",
                   prefixIcon: Icon(Icons.email)),
@@ -60,7 +73,12 @@ class _AddScreenState extends State<AddScreen> {
               height: 15,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _validateNameEmpty = _textNameController.text.isEmpty;
+                  _validateMobileEmpty = _textMobileController.text.isEmpty;
+                });
+              },
               style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.indigoAccent),
               child: const Text("Add Contact"),
